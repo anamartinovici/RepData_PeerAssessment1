ba651862ad58e463dfb4d00a7520c1d0e68030d5
maglet <tobin.magle@ucdenver.edu>
2015-12-18 09:47:08 -0700

    Start project
    Extracted the activity file and created week2code.R with a scaffold for
    the code. Will create .Rmd file later

---
+++ week2code.R
@@ -0,0 +1,31 @@
+#load the data
+steps<-read.csv("activity.csv")
+
+#Make a histogram of total steps per day
+hist(steps$steps, xlab="Day", main="Steps per day", ylab="Steps")
+
+#Calculate the mean steps
+mean<-mean(steps$steps, na.rm=TRUE)
+
+#Calculate the median steps
+median<-median(steps$steps, na.rm=TRUE)
+
+#Make a time series plot
+
+#Which 5 minute interval across days contains max steps
+
+#Total number of missing values
+
+#Fill in missing values
+
+#Create new dataset with values filled
+
+#Make a histogram of the total number of steps taken each day and 
+  #Calculate and report the mean and median total number of steps taken per day. 
+  #Do these values differ from the estimates from the first part of the assignment? 
+  #What is the impact of imputing missing data on the estimates of the total daily number of steps?
+
+#Create a new factor variable in the dataset with two levels -- "weekday" and "weekend" 
+#indicating whether a given date is a weekday or weekend day.
+
+#Make a panel plot containing time series of 5 min interval and average number of steps taken on weekends and weekdays
